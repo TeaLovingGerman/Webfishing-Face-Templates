d02a44c33b38b032ea71c728a8ff01a08fa2880d
Tea-Loving German <alexander.hnrch@gmail.com>
2024-11-19 20:17:08 +0100

    Proper setup after testing some things

--- GDWeave/mods/AuthorName.ModID/main.gd
+++ GDWeave/mods/AuthorName.ModID/main.gd
@@ -8,6 +8,3 @@ func _ready():
 		Lure.add_content(id, "template_eyes", "mod://Resources/Cosmetics/Eyes/template_eyes.tres", [Lure.FLAGS.FREE_UNLOCK])
 		Lure.add_content(id, "template_nose", "mod://Resources/Cosmetics/Noses/template_nose.tres", [Lure.FLAGS.FREE_UNLOCK])
 		Lure.add_content(id, "template_mouth", "mod://Resources/Cosmetics/Mouths/template_mouth.tres", [Lure.FLAGS.FREE_UNLOCK])
-		Lure.add_content(id, "inrotationtrue_eyes", "mod://Resources/Cosmetics/Eyes/inrotationtrue_eyes.tres", [Lure.FLAGS.FREE_UNLOCK])
-		Lure.add_content(id, "inrotationfalse_eyes", "mod://Resources/Cosmetics/Eyes/inrotationfalse_eyes.tres", [Lure.FLAGS.FREE_UNLOCK])
-		Lure.add_content(id, "templatebig_mouth", "mod://Resources/Cosmetics/Eyes/templatebig_mouth.tres", [Lure.FLAGS.FREE_UNLOCK])
